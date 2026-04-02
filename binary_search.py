9a822018d7d3071e873d0d3342f3c88ef7493515
Ming Li <ming.george.li@gmail.com>
2026-04-02 12:24:40 +0100

    fix

--- binary_search.py
+++ binary_search.py
@@ -6,27 +6,28 @@ toy binary search implementation
 
 
 def binary_search(arr, target):
-    """binary search implementation on a sorted array."""
     i = 0
     j = len(arr) - 1
+
+    # binary search only operates on sorted array.
+    arr = sorted(arr)
 
     while i < j:
         mid = (i + j) // 2
 
-        if arr[mid] == target:
-            return mid
-
         if arr[mid] < target:
-            i += 1
+            i = mid + 1
+        elif arr[mid] > target:
+            j = mid
         else:
-            j -= 1
+            return mid
 
     return -1
 
 
 if __name__ == "__main__":
     arr = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-    target = 34
+    target = 35
     result = binary_search(arr, target)
 
     print(result)
